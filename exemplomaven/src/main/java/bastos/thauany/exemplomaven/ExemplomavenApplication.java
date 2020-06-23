cffb5ce2ccbe7bc7df0caf753af1f41a7b391cea
ThauanyBastos <thauanyb267@gmail.com>
2020-06-23 15:20:51 -0300

    First commit exemplomaven

---
+++ exemplomaven/src/main/java/bastos/thauany/exemplomaven/ExemplomavenApplication.java
@@ -0,0 +1,15 @@
+package bastos.thauany.exemplomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplomavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplomavenApplication.class, args);
+
+		System.out.println("OOOOI... Spring boot com maven");
+	}
+
+}
