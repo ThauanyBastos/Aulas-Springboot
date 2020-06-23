5b6310eb37cab6183296865c149ac6a516acadae
ThauanyBastos <thauanyb267@gmail.com>
2020-06-23 15:19:23 -0300

    First commit exemplogradlehelloworld

---
+++ exemplogradlehelloworld/exemplogradlehelloworld/src/test/java/bastos/thauany/exemplogradlehelloworld/ExemplogradlehelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package bastos.thauany.exemplogradlehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplogradlehelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
