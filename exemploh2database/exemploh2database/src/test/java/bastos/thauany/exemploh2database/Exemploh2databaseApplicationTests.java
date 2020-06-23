fa2437549f47c1e9962db7b3cd854113442ddb56
ThauanyBastos <thauanyb267@gmail.com>
2020-06-23 15:20:11 -0300

    First commit exemploh2database

---
+++ exemploh2database/exemploh2database/src/test/java/bastos/thauany/exemploh2database/Exemploh2databaseApplicationTests.java
@@ -0,0 +1,13 @@
+package bastos.thauany.exemploh2database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemploh2databaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
