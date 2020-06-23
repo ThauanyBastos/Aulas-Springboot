d5767f60714d221401e3c0065c4960d993df9913
ThauanyBastos <thauanyb267@gmail.com>
2020-06-23 15:18:40 -0300

    First commit exemplogradle

---
+++ exemplogradle/exemplogradle/src/main/java/bastos/thauany/exemplogradle/ExemplogradleApplication.java
@@ -0,0 +1,15 @@
+package bastos.thauany.exemplogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplogradleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExemplogradleApplication.class, args);
+		System.out.println("oiiii... spring boot com gradle");
+	}
+
+}
